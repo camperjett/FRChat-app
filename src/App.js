1984f7ce90471dff93d2cee5466b07808bcc6cb3
Jaideep Sharma <jaideep.sharma.cse20@itbhu.ac.in>
2022-04-20 01:52:37 +0530

    v1.0.1

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function SignOut(){
 function ChatRoom(){
   const dummy = useRef();
   const messageRef = firestore.collection('messages');
-  const query = messageRef.orderBy('createdAt').limit(25);
+  const query = messageRef.orderBy('createdAt');
   const [messages]  = useCollectionData(query, {idField: 'id'});
   const [formValue, setFormValue] = useState('');
   const sendMessage = async(e) => {
@@ -73,10 +73,10 @@ function ChatRoom(){
   return(
     <>
       <main>
+        <div ref={dummy}></div>
         <div>
           {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
         </div>
-        <div ref={dummy}></div>
       </main>
       <form onSubmit={sendMessage}>
         <input value={formValue} onChange={e=>setFormValue(e.target.value)}/>
